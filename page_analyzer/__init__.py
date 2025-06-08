8b71401610785510b09345ad518d6f1b537015cd
Nikolai Tenengolts <ntenengolts@gmail.com>
2025-06-09 01:08:18 +0300

    fix14 step 3

--- page_analyzer/__init__.py
+++ page_analyzer/__init__.py
@@ -2,8 +2,6 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 
-from .app import index, add_url, show_url, show_urls, test # noqa: F401
-
 
 load_dotenv()
 
@@ -11,4 +9,6 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'secret')
 DATABASE_URL = os.getenv('DATABASE_URL')
+
+from .app import index, add_url, show_url, show_urls, test # noqa: F401
 
