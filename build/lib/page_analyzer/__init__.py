031b9a7b6fe8a269a25e7405d5c7b78bdcd5f3d7
Nikolai Tenengolts <ntenengolts@gmail.com>
2025-06-08 23:06:57 +0300

    Fix6 Step 3

--- build/lib/page_analyzer/__init__.py
+++ build/lib/page_analyzer/__init__.py
@@ -1,7 +1,6 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
-from . import app
 
 
 load_dotenv()
--- page_analyzer/__init__.py
+++ page_analyzer/__init__.py
@@ -2,11 +2,9 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'secret')
 DATABASE_URL = os.getenv('DATABASE_URL')
-
